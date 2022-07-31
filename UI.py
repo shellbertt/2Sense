3eb0e4d715448a1f9c74047a639254979eb35253
rumikam <rumika.mascarenhas@ucalgary.ca>
2022-07-31 17:03:38 -0600

    changing frames

--- UI.py
+++ UI.py
@@ -11,28 +11,60 @@ import time
 pygame.mixer.init()
 
 window = Tk()
-
 window.geometry("1000x600")
 window.title("2Sense_Stimulation")
 
-window.overrideredirect(1)
+window.overrideredirect(1) #borderless window
 
 bg = PhotoImage(file = "assets/LogoBanner.png") #adding an image as a background for later
 
 Width = 1000
 Height = 600
 
-var = StringVar()
-label = Label(window, textvariable=var, relief=RAISED, padx=20, pady=20)
+Logo = Label(window, image = bg)
+Logo.pack()
 
-var.set("Instructions:" + "\n" + "\n" + "No Stimulation: No visual stimulation; foot fall does NOT start vibration" + "\n" + "Visual Stimulation only: Visual stimulation turned ON; foot fall does NOT start vibration" + "\n" +
-            "Vibration only: No visual stimulation; foot fall triggers vibration on ankle" + "\n" + "Both: Visual stimulation ON; foot fall triggers vibration on ankle")
-label.place(x=250, y=200)
+text1 = StringVar()
+NameLabel = Label(window, textvariable=text1, padx=20, pady=20)
+
+text1.set("Enter Name of Participant:")
+NameLabel.pack()
+
+ID=StringVar()
+UserName_input = Entry(window,bd=4,textvariable=ID)
+UserName_input.pack()
+
+Button_submit = Button(window, text = "SUBMIT", command=lambda: (UserName_input.pack_forget(), NameLabel.pack_forget(),Button_submit.pack_forget(), StimFrame()))
+Button_submit.pack()
 
 stim = main.controller()
 
-#introFrame = Frame(window, width=Width, height=Height, bg="white")
-#introFrame.pack()
+def StimFrame():
+    
+    #Takes user to the Stimulation window when "Submit" is pressed
+
+    Username = UserName_input.get()
+    
+    text2 = StringVar()
+    NameDisplay = Label(window, textvariable=text2, padx=10,pady=20)
+
+    text2.set("Participant Name: " + Username )
+    NameDisplay.place(x=25, y=150)
+
+
+    text3 = StringVar()
+    Instruct = Label(window, textvariable=text3, relief=RAISED, padx=20, pady=20)
+
+    text3.set("Instructions:" + "\n" + "\n" + "No Stimulation: No visual stimulation; foot fall does NOT start vibration" + "\n" + "Visual Stimulation only: Visual stimulation turned ON; foot fall does NOT start vibration" + "\n" +
+                 "Vibration only: No visual stimulation; foot fall triggers vibration on ankle" + "\n" + "Both: Visual stimulation ON; foot fall triggers vibration on ankle")
+    Instruct.place(x=250, y=200)
+
+
+    NoStimButton = Button(window, text = "No Stimulation", command=NoStimulation).place(x=25, y=400)
+    VisButton = Button(window, text = "Visual Only", command=VisualOnly).place(x=300, y=400)
+    VibButton = Button(window, text = "Vibration Only", command=VibrationOnly).place(x=600, y=400)
+    BothButton = Button(window, text = "Both On", command=BothOn).place(x=900, y=400)
+    StopButton = Button(window, text="Stop Stimulation", command=StopStim).place(x=450, y=500)
 
 def playBeep():
     pygame.mixer.music.load("assets/beep.wav")
@@ -61,19 +93,7 @@ def StopStim():
     print('wd')
     exit()
     print('?')
-#add stuff to stop stimulation
 
-# label with the logo is on the second frame 
-label = Label(window, image = bg)
-label.place(x=0, y=25)
-
-#introLabel= Label(introFrame, )
-
-NoStimButton = Button(window, text = "No Stimulation", command=NoStimulation).place(x=25, y=400)
-VisButton = Button(window, text = "Visual Only", command=VisualOnly).place(x=300, y=400)
-VibButton = Button(window, text = "Vibration Only", command=VibrationOnly).place(x=600, y=400)
-BothButton = Button(window, text = "Both On", command=BothOn).place(x=900, y=400)
-StopButton = Button(window, text="Stop Stimulation", command=StopStim).place(x=450, y=500)
 
 while True:
     stim.iterate()
