032becf9396ed37e452cfd98f85ebea5c24b3f6d
Kaaden RumanCam <kaadenruman@hotmail.com>
2022-07-30 21:58:59 -0600

    thingy to generate, draw, and update many triangles

--- triangle.py
+++ triangle.py
@@ -5,11 +5,11 @@ from math import cos, pi, sin
 class triangle():
     # length from centre to point
     # rotation in [0, 1]
-    def __init__(self, canvas, x, y, length, rotation, colour='white'):
+    def __init__(self, canvas, x, y, radius, rotation, colour='white'):
         self.canvas = canvas
         self.x = x
         self.y = y
-        self.length = length
+        self.radius = radius
         self.radians = rotation * 2 * pi
         self.colour = colour
 
@@ -17,12 +17,12 @@ class triangle():
         r1 = self.radians
         r2 = self.radians + 2 * pi / 3
         r3 = self.radians + 4 * pi / 3
-        self.canvas.create_polygon(self.x + self.length * cos(r1), \
-                self.y + self.length * sin(r1), \
-                self.x + self.length * cos(r2), \
-                self.y + self.length * sin(r2), \
-                self.x + self.length * cos(r3), \
-                self.y + self.length * sin(r3), fill=self.colour)
-    def rotate(self, rotation):
-        self.radians += rotation * 2 * pi
+        self.canvas.create_polygon(self.x + self.radius * cos(r1), \
+                self.y + self.radius * sin(r1), \
+                self.x + self.radius * cos(r2), \
+                self.y + self.radius * sin(r2), \
+                self.x + self.radius * cos(r3), \
+                self.y + self.radius * sin(r3), fill=self.colour)
+    def rotate(self, amount):
+        self.radians += amount * 2 * pi * (1 if self.radians > 0 else -1)
 
---
+++ visual.py
@@ -0,0 +1,36 @@
+import triangle
+from tkinter import *
+from random import randint, random
+
+class controller():
+    def __init__(self, window, period):
+        self.window = window
+        screenh = window.winfo_screenheight()
+        screenw = window.winfo_screenwidth()
+        self.canvas = Canvas(window, bg='black', height=screenh, width=screenw)
+        self.canvas.pack()
+        self.period = period
+        # balance number of triangles with their size so everything fits reasonably
+        self.count = 500
+        self.radius = int((screenw * screenh // self.count) ** .5)
+        print(screenw, screenh, self.count, 'initr', self.radius)
+        self.horizontal_count = screenw // self.radius
+        self.vertical_count = screenh // self.radius
+        self.count = self.horizontal_count * self.vertical_count
+        # assure a minimum size
+        while self.radius < 40:
+            self.count -= 10
+            self.radius = screenw * screenh // self.count
+            self.horizontal_count = screenw // self.radius
+            self.vertical_count = screenh // self.radius
+            self.count = self.horizontal_count * self.vertical_count
+        self.triangles = [triangle.triangle(self.canvas, x * self.radius + randint(-10, 10) + self.radius // 2 , y * self.radius + randint(-10, 10) + self.radius // 2, (self.radius - 10) // 2, random() - .5) for y in range(self.vertical_count + 1) for x in range(self.horizontal_count + 1)]
+
+    def iterate(self):
+        for t in self.triangles:
+            t.draw()
+        self.canvas.update()
+        for t in self.triangles:
+            t.rotate(self.period)
+        self.canvas.delete('all')
+
