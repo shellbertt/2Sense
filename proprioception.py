71284da8ed3ca8da0a20b161bbe1afcd9d59fc61
Kaaden RumanCam <kaadenruman@hotmail.com>
2022-07-31 19:55:55 -0600

    fine tune vibrator (it wasnt working)

--- proprioception.py
+++ proprioception.py
@@ -4,21 +4,34 @@ import pyfirmata
 import time
 
 class controller():
-    def __init__(self):
+    # mark_step notes the stimulus in the data
+    def __init__(self, mark_step):
+        self.mark_step = mark_step
         self.board = pyfirmata.Arduino('COM5')
         self.it = pyfirmata.util.Iterator(self.board)
         self.it.start()
         self.board.analog[0].enable_reporting()
+        self.wait = 15 # wait some cycles inbetween activation
+        self.waited = 15
 
     def iterate(self, on):
         reading = self.board.analog[0].read()
         if reading is None: return # first reading may be None, can just ignore it
-        if on:
-            self.board.digital[8].write(1 if reading > .9 else 0)
+        if reading > .9 and on and self.waited >= self.wait:
+            self.waited = 0
+            self.mark_step(1)
+            self.board.digital[8].write(1)
+        else:
+            self.board.digital[8].write(0)
+            if self.waited < 15:
+                self.waited += 1
+
+def passer(trash):
+    pass
 
 if __name__ == '__main__':
-    c = controller()
+    c = controller(passer)
     while True:
-        c.iterate()
+        c.iterate(True)
         time.sleep(.15)
 
